41f9322bfb064ef0d6724de9c55bc4394f8e1d19
francopachec0 <francopacheco.dev@gmail.com>
2022-01-18 01:24:41 -0300

    feat:finished

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const displayValorAnterior = document.getElementById('valor_anterior');
+const displayValorActual = document.getElementById('valor_actual');
+
+const botonesNumeros = document.querySelectorAll('.number');
+const botonesOperadores = document.querySelectorAll('.operator');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.agregarNumero(boton.innerHTML);
+    });
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.computar(boton.value);
+    });
+});
+
